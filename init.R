658f4e1f69c928baf87316fe58b127558cf5d643
X80110 <xbollo@gmail.com>
2020-12-17 01:03:31 +0100

    added packages to install | Modified README

---
+++ init.R
@@ -0,0 +1,9 @@
+my_packages = c("tidyverse", "readxl", "countrycode","scales")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
