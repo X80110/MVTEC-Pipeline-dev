80b43d8e7e87ba65425412c696c53bcefd498ff5
melondulzon <melondulzon@gmail.com>
2020-12-29 17:02:03 +0100

    all outputs except main is formated as csv, not file

--- Rscripts/xavier_dataprep.R
+++ Rscripts/xavier_dataprep.R
@@ -185,23 +185,12 @@ merged_dataset <- select(dd, -continent) %>%
       
 
 # Set output to be captured by python ----
+cat(format_csv(merged_dataset))
+
+
+
 # USING HEROKU EPHEMERAL SYSTEM -> https://stackoverflow.com/questions/12416738/how-to-use-herokus-ephemeral-filesystem
 #setwd(file.path(getwd(), fsep = .Platform$file.sep))
-
-library(gapminder)
-cat(format_csv(merged_dataset))
-gap_with_colors <-
-  data.frame(merged_dataset,
-             cc = I(country_colors[match(merged_dataset$location,
-                                         names(country_colors))]))
-
-# bubble plot, focus just on Africa and Europe in 2007
-keepers <- with(gap_with_colors,
-                continent %in% c("Africa", "Europe") & year == 2007)
-plot(lifeExp ~ gdpPercap, gap_with_colors,
-     subset = keepers, log = "x", pch = 21,
-     cex = sqrt(gap_with_colors$pop[keepers]/pi)/1500,
-     bg = gap_with_colors$cc[keepers])
 
 
 # for (i in merged_dataset) {
