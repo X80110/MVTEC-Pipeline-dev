0e6928e87828c92e77b3cba0fd896cfa7fec1bdc
Spe Chen Yu-Syuan <spechen@sph.com.sg>
2021-01-04 23:59:57 +0800

    simplify and test

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,8 +1,5 @@
 import requests
 import pandas as pd
-from datetime import datetime
-import pytz
-import glob
 
 # scraping currency table 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
@@ -12,22 +9,14 @@ response = requests.get(url, headers=headers)
 dfs = pd.read_html(response.text)
 df = dfs[0]
 
-# getting currency stored previously
-for name in glob.glob('*[0-9].*'): #get filenames contain digit
-    currency_filename = name
-currency_df = pd.read_csv(currency_filename)
+# read the historical file
+currency_df = pd.read_csv('currency_output.csv')
 
-# updating
+# updating new date's rate
 combined = pd.concat([df, currency_df])
 combined['Date'] = pd.to_datetime(combined['Date'])
 combined = combined.drop_duplicates(subset='Date', keep="first")
 
-# save it with timestampe of date
-tz_md = pytz.timezone('Europe/Madrid') 
-date_str = datetime.now(tz_md).strftime("%Y%m%d")
-print("Madrid date now:", date_str)
-
-combined.to_csv('output' + date_str + '.csv', index=False)
-
+combined.to_csv('currency_output.csv', index=False)
 print("updated currency table successfully")
 
