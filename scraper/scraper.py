0aaad5523d0e1349aa648934468f4131a54a2826
Spe Chen Yu-Syuan <spechen@sph.com.sg>
2021-01-06 00:15:01 +0800

    testing for scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from io import StringIO
 
 # scraping currency table 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
@@ -17,7 +18,12 @@ if response.status_code == 200:
     combined = pd.concat([df, currency_df])
     combined['Date'] = pd.to_datetime(combined['Date'])
     combined = combined.drop_duplicates(subset='Date', keep="first")
-    combined.to_csv('currency_output.csv', index=False)
+
+    # output = StringIO()
+    # combined.to_csv(output)
+    # print(output.getvalue())
+
+    # combined.to_csv('currency_output.csv', index=False)
     print("updated currency table successfully")
 else: 
     print("data source is not responding. we will use archive csv.")
